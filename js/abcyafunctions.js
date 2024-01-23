d90db1e070d43d6c289e03815e92e4191e7eced0
TheGreatMaximus98 <87099103+TheGreatMaximus98@users.noreply.github.com>
2024-01-22 20:04:44 -0500

    Create abcyafunctions.js

---
+++ js/abcyafunctions.js
@@ -0,0 +1,27 @@
+
+function showExitDialog() {
+    var d = document.getElementById("confirmWrapper");
+    d.style.display = "block";
+}
+function exitGame() {
+    window.top.abcyaExit();
+    //window.location.URL = "../../";
+}
+
+function hideExitDialog() {
+    var dialog = document.getElementById("confirmWrapper");
+    dialog.style.display = "none";
+}
+
+function initButtons() {
+    var device = new Phaser.Device();
+    if (!device.desktop) {
+        document.getElementById("btnExit").onmousedown = showExitDialog;
+        document.getElementById("btnYes").onmousedown = exitGame;
+        document.getElementById("btnNo").onmousedown = hideExitDialog;
+        document.getElementById("exitButton").style.display = "block";
+    }else{
+        document.getElementById("btnExit").style.display = "none";
+    }
+
+}
